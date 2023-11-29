7f0e2823119b0672ab60b312ef83a9126492facc
Cody <codywalenciak@gmail.com>
2023-11-29 14:23:47 -0600

    Add completed tokenize function

--- functions.c
+++ functions.c
@@ -8,15 +8,28 @@
 
 char **tokenize(char *str, char *delim)
 {
-    size_t buffsize = 512;
-    char *temp = malloc(buffsize * sizeof(char));
-    char *tokens[1024];
+	size_t buffsize = 512;
+	char *temp = malloc(buffsize * sizeof(char));
+	char *tokens[1024];
+	int i = 0;
 
-    if (str == NULL)
-    {
-        exit(1);
-    }
+	if (str == NULL)
+	{
+		exit(1);
+	}
 
+	temp = strtok(str, delim);
+	while (temp != NULL)
+	{
+		tokens[i] = temp;
+		temp = strtok(0, delim);
+		i++;
+	}
+	tokens[i] = NULL;
+	i--;
+	if (tokens[i][strlen(tokens[i] - 1)] == '\n')
+		tokens[i][strlen(tokens[i] - 1)] = '\0';
+
+	return (tokens);
 
 }
-
