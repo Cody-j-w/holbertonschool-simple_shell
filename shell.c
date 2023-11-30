af4c4069f939eb799f689fbf8fdfe8fdcabde738
Cody <codywalenciak@gmail.com>
2023-11-30 14:23:15 -0600

    Core of main function

--- shell.c
+++ shell.c
@@ -10,6 +10,13 @@ int main(void)
 {
     size_t buffsize = 1024;
     char *buffer = malloc(buffsize * sizeof(char));
+    char *path = malloc(buffsize * sizeof(char));
+    char **paths;
+    char **tokens;
+    extern char **environ;
+
+    path = get_path(environ);
+    paths = tokenize(path, ":");
 
     if (buffer == NULL)
     {
@@ -19,7 +26,20 @@ int main(void)
     while (!feof(stdin))
     {
         printf("$ ")
+        getline(&buffer, &buffsize, stdin);
+        if (exit_check(buffer) == 1)
+        {
+            break;
+        }
+        else
+        {
+            tokens = tokenize(buffer, " ");
+            forkit(paths, tokens);
+        }
     }
+    free(path);
+    free(paths);
     free(buffer);
+    free(tokens);
     exit(0);
 }
