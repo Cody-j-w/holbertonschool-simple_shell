5cd57d555ca67c9f0f4b4165014f43b8c6f01d7c
Cody <codywalenciak@gmail.com>
2023-12-07 16:17:41 -0600

    Account for fork-to-main status pipeline

--- shell.c
+++ shell.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	size_t buffsize = 1024;
+	int status = 0;
 	char *buffer = malloc(buffsize * sizeof(char));
 	char *path = NULL, *strcheck = NULL, **paths = NULL, **tokens = NULL;
 
@@ -29,7 +30,9 @@ int main(int argc, char *argv[], char *envp[])
 		{
 			tokens = tokenize(strcheck, " ");
 			fflush(stdout);
-			forkit(paths, tokens, envp, argv[0], argc);
+			status = forkit(paths, tokens, envp, argv[0], argc);
+			if (status != 0)
+				break;
 			free_array(tokens);
 			tokens = NULL;
 		}
@@ -42,7 +45,7 @@ int main(int argc, char *argv[], char *envp[])
 	free(strcheck);
 	if (path != NULL)
 		free(path);
-	exit(0);
+	exit(status);
 }
 
 int path_check(char *str)
